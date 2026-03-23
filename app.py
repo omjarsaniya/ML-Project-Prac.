65c5fa4607193ae5a6df0919f34b72bbb7c85a51
Om Jarsaniya <ombjarsaniya123@gmail.com>
2026-03-23 14:37:13 +0530

    Data Ingition

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import sys
-
 from src.mlproject.logger import logging
-
 from src.mlproject.exception import CustomException
-
+from src.mlproject.components.data_ingition import DataIngestion
+from src.mlproject.components.data_ingition import DataIngestionConfig
 
 
 if __name__ == "__main__":
     logging.info("The execution has started")
 
     try:
-        a = 1 / 0
+        #data_ingestion_config = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+        
     except Exception as e:
         logging.info("An exception has occurred")
         raise CustomException(e, sys)
+    
+    
+
     
